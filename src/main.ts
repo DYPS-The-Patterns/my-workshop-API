860e679bb97048cf7119b43729837163983ceedd
Christian C <geolrco124@hotmail.com>
2022-06-08 22:30:13 -0500

    feat: Added Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My Workshop')
+    .setDescription('My Workshop API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(3000);
 }
 bootstrap().then();
