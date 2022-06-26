8a85a1eb599a4f47466d1363bd82ea03f3973fd5
ChicoRipCurl <chicoripcurl1@gmail.com>
2022-06-26 01:31:46 -0500

    feat(chats): Added Layers components.

--- src/chats/interface/rest/chats.controller.ts
+++ src/chats/interface/rest/chats.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ChatsService } from '../../domain/services/chats.service';
 
 @Controller('chats')
-export class ChatsController {}
+export class ChatsController {
+  constructor(private chatsService: ChatsService) {}
+
+  @Get()
+  async getAllChats() {
+    const chats = await this.chatsService.getAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Chats fetched succesfully',
+      chats,
+    };
+  }
+}
