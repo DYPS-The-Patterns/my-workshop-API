9037a693db5e384e3cbbe83a8bb54e827193b82a
iceberg <geolrco124@hotmail.com>
2022-06-25 21:01:23 -0500

    feat: Implementation of DTOs

---
+++ src/profiles/application/transform/dto/update-owner.dto.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateOwnerDto } from './update-owner.dto';
+
+describe('UpdateOwnerDto', () => {
+  it('should be defined', () => {
+    expect(new UpdateOwnerDto()).toBeDefined();
+  });
+});
